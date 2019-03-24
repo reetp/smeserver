ac08aa3361e7a168771f5af70bcaa396bb2f770f
Michael J McCarn <mmccarn-github@mmsionline.us>
2019-03-24 09:32:22 -0400

    + accept hosts on the command line + include certificate issuer /O= in output to aid in renewal

--- bin/ssl-check.sh
+++ bin/ssl-check.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 # for host in 192.168.200.2 192.168.200.167 192.168.200.197 192.168.200.14
-for host in office cloud docker neth mattermost
+HOSTS=${*-office cloud docker neth mattermost}
+for host in $HOSTS
 do
-  printf "%-12s" "$host"
-  expires=$(echo | \
+  printf "%-20s" "$host"
+  sslinfo=$(echo | \
     openssl s_client -servername $host -connect $host:443 2>/dev/null | \
-    openssl x509 -noout -dates | \
-    grep notAfter |\
-    sed 's/notAfter=//')
+    openssl x509 -noout -dates -issuer 2>/dev/null)
+
+  issuer=$(echo $sslinfo  |\
+    sed 's/.*issuer.*O=\([^\/]*\).*/\1/')
+  printf "%-20s" "$issuer"
+
+  expires=$(echo $sslinfo |\
+    sed 's/.*notAfter=\(.*\) issuer.*/\1/')
+
   expiress=$(date -d $(date -d "${expires}" '+%Y%m%d') '+%s')
   today=$(date -d $(date '+%Y%m%d') '+%s')
   diffdays=$(( ($expiress - $today) / (60*60*24) ))
@@ -17,6 +24,6 @@ do
   else
     renew=""
   fi
-  echo $(date -d "${expires}" '+%Y%m%d') \($diffdays days\) $renew
+  printf "$(date -d "${expires}" '+%Y%m%d') ($diffdays days)\t$renew\n"
 
 done
